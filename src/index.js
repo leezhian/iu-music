64d21b5675d1b5266c42354c87da69a151653eb5
kami232 <1347408207@qq.com>
2020-02-03 23:20:59 +0800

    feat: 新增vconsole调试

--- src/index.js
+++ src/index.js
@@ -10,15 +10,26 @@ Vue.config.devtools = true; // 用于调试
 import App from './App';
 // 处理移动端300毫秒延迟
 import fastclick from 'fastclick';
+// 懒加载
+import VueLazyload from "vue-lazyload";
+// 加密
+import rsa from 'static/js/rsa';
+
 import router from './router/index';
 import store from './store';
 // 全局样式
 import './static/scss/reset.scss';
-// 懒加载
-import VueLazyload from "vue-lazyload";
+
 import loadImg from 'static/images/default.jpg';
-// 加密
-import rsa from 'static/js/rsa';
+
+
+import Vconsole from 'vconsole';
+
+const devMode = process.env.NODE_ENV !== 'production';
+if (devMode) {
+  const vconsole = new Vconsole();
+  Vue.use(vconsole);
+}
 
 rsa.install(Vue);
 
